898abe8a9356e2b85db1ee16294a1ca06bacdfbb
Cynthia Wei <cynthiawei1209@gmail.com>
2025-06-20 10:40:24 -0400

    Update rasp_lazy_susan.py

--- lazy-susan/rasp_lazy_susan.py
+++ lazy-susan/rasp_lazy_susan.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-MOTOR_PIN = 24  # BCM pin number
+LS_PIN = 24  # BCM pin number
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_PIN, GPIO.OUT)
+GPIO.setup(LS_PIN, GPIO.OUT)
 
 # 1 kHz PWM
-pwm = GPIO.PWM(MOTOR_PIN, 1000)
+pwm = GPIO.PWM(LS_PIN, 1000)
 # duty-cycle = (100/255)*100 ≈ 39%
 pwm.start(39.2)
 
