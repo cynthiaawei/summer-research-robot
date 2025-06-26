02693c93d51fbe453fd63d92c3d9f77a38c335ad
Jenny Chung <jennyy.chung@mail.utoronto.ca>
2025-06-26 16:25:09 -0400

    push test

---
+++ test-imports.py
@@ -0,0 +1,12 @@
+import asyncio
+import re
+import threading
+import keyboard
+import subprocess
+import time
+import platform
+import speech_recognition as sr
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+import RPi.GPIO as GPIO
+import logging
