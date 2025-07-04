7a5f0f655651106448d8d3de152a5d6a5d73e101
Krrish Sharma <sharmakrrish50@gmail.com>
2025-07-04 15:28:26 -0400

    HELP

--- robot/src/App.tsx
+++ robot/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import NavButtons from './components/navbuttons';
 import TextButton from './components/textbutton';
 import Speech from './components/speech';
@@ -7,15 +7,13 @@ import ArrowKeys from './components/arrow_keys';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<NavButtons />} />
         <Route path="/text" element={<TextButton />} />
         <Route path="/speech" element={<Speech />} />
         <Route path="/arrow-keys" element={<ArrowKeys />} />
       </Routes>
-    </BrowserRouter>
   );
-};
+}; 
 
 export default App;
