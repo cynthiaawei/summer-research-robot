6253c1e7b479db3de48898b4627f5db27c919250
Krrish Sharma <sharmakrrish50@gmail.com>
2025-07-10 14:05:40 -0400

    Made minor changes

--- robot/src/main.tsx
+++ robot/src/main.tsx
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
