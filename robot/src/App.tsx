837c423fa0add8bb9ca82217e3ce38a7d35a64f8
Krrish Sharma <93853732+rikidoufox@users.noreply.github.com>
2025-07-11 11:52:20 -0400

    Update App.tsx

--- robot/src/App.tsx
+++ robot/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { UserProvider } from './components/UserContext';
 import FaceRecognitionGate from './components/facerecognition';
 import NavButtons from './components/navbuttons';
 import TextButton from './components/textbutton';
@@ -13,25 +12,26 @@ const App: React.FC = () => {
   
   return (
     <Router>
-      <UserProvider>
-        <div style={{ minHeight: '100vh', width: '100vw' }}>
-          <Routes>
-            {/* Face Recognition Gate - Entry Point */}
-            <Route path="/" element={<FaceRecognitionGate />} />
-            
-            {/* User Registration - When face recognition fails after 3 attempts */}
-            <Route path="/register" element={<RegistrationPage />} />
-            
-            {/* Main Menu - After successful face recognition or registration */}
-            <Route path="/menu" element={<NavButtons />} />
-            
-            {/* Control Interfaces */}
-            <Route path="/text" element={<TextButton />} />
-            <Route path="/speech" element={<Speech />} />
-            <Route path="/arrow-keys" element={<ArrowKeys />} />
-          </Routes>
-        </div>
-      </UserProvider>
+      <div style={{ minHeight: '100vh', width: '100vw' }}>
+        <Routes>
+          {/* Face Recognition Gate - Entry Point */}
+          <Route path="/" element={<FaceRecognitionGate />} />
+          
+          {/* User Registration - When face recognition fails after 3 attempts */}
+          <Route path="/register" element={<RegistrationPage />} />
+          
+          {/* Main Menu - After successful face recognition or registration */}
+          <Route path="/menu" element={<NavButtons />} />
+          
+          {/* Control Interfaces */}
+          <Route path="/text" element={<TextButton />} />
+          <Route path="/speech" element={<Speech />} />
+          <Route path="/arrow-keys" element={<ArrowKeys />} />
+          
+          {/* Legacy route compatibility */}
+          <Route path="/user" element={<ArrowKeys />} />
+        </Routes>
+      </div>
     </Router>
   );
 }; 
